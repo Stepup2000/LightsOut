f25d85ab3356d9dba474e25f2a8826d129b822a9
Kevin <kevmensink@hotmail.com>
2021-09-14 18:35:16 +0200

    Audio for MainGameUI and MiniGameUI

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -8,19 +8,21 @@ public class MenuButtons : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
-    public static void StartGame()
+    public void StartGame()
     {
         SceneManager.LoadScene("City", LoadSceneMode.Single);
+
+
     }
 
-    public static void GoToMenu()
+    public void GoToMenu()
     {
         SceneManager.LoadScene("Main", LoadSceneMode.Single);
     }
 
-    public static void QuitGame()
+    public void QuitGame()
     {
         Application.Quit();
     }
@@ -28,6 +30,6 @@ public class MenuButtons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
